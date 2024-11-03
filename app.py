22cbf97bf22dc65592fbd64241772d4b03057ebd
leomsafreire <7leomartins7@gmail.com>
2024-11-03 20:48:37 -0300

    first commit :)

---
+++ app.py
@@ -0,0 +1,156 @@
+import streamlit as st
+from utils import (
+    add_vintage_background,
+    add_background_music,
+    calculate_xT,
+    crop_figure,
+    load_and_resize_image,
+    get_competitions,
+    get_player_events_competition,
+    plot_passes,
+    plot_carries,
+    plot_reception_actions,
+    plot_shots
+)
+from statsbombpy import sb
+
+
+
+def main():
+    st.set_page_config(layout="wide")   
+    add_vintage_background()
+    st.title('Brasil 70')
+    
+
+    players = {
+        'Ado': ('Eduardo Roberto Stinghen', 'https://upload.wikimedia.org/wikipedia/commons/8/82/Eduardo_Roberto_Stinghen.jpg'),
+        'Baldocchi': ('José Guilherme Baldocchi', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Baldocchi_%281970%29.jpg/227px-Baldocchi_%281970%29.jpg'),
+        'Brito': ('Hercules Brito Ruas', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/H%C3%A9rcules_de_Brito_Ruas.jpg/330px-H%C3%A9rcules_de_Brito_Ruas.jpg'),
+        'Caju': ('Paulo Cézar Lima', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Caju_1978.jpg/1200px-Caju_1978.jpg'),
+        'Carlos Alberto': ('Carlos Alberto Torres', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Carlos_Alberto_%281970%29.jpg/220px-Carlos_Alberto_%281970%29.jpg'),
+        'Clodoaldo': ('Clodoaldo Tavares de Santana', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Clodoaldo_1970.jpg/220px-Clodoaldo_1970.jpg'),
+        'Dada Maravilha': ('Dario José dos Santos', 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Dad%C3%A1_Maravilha_%281970%29.jpg'),
+        'Edu': ('Jonas Eduardo Américo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Edu_1970.jpg/220px-Edu_1970.jpg'),
+        'Émerson Leão': ('Emerson Leão', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/%C3%89merson_Le%C3%A3o.jpg/220px-%C3%89merson_Le%C3%A3o.jpg'),
+        'Everaldo': ('Everaldo Marques da Silva', 'https://upload.wikimedia.org/wikipedia/commons/2/20/Everaldo_Marques_da_Silva.jpg'),
+        'Félix': ('Félix Miéli Venerando', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/F%C3%A9lix_Brasil.jpg/220px-F%C3%A9lix_Brasil.jpg'),
+        'Fontana': ('José de Anchieta Fontana', 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Jos%C3%A9_de_Anchieta_Fontana_%281970%29.jpg'),
+        'Gérson': ('Gérson de Oliveira Nunes', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/G%C3%A9rson_1970.jpg/1200px-G%C3%A9rson_1970.jpg'),
+        'Jairzinho': ('Jair Ventura Filho', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Jairzinho_%28Jair_Ventura_Filho%2C_1970%29.jpg/150px-Jairzinho_%28Jair_Ventura_Filho%2C_1970%29.jpg'),
+        'Joel Camargo': ('Joel Camargo', 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Joel_Camargo_%281970%29.jpg'),
+        'Marco Antônio': ('Marco Antônio Feliciano', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Marco_Ant%C3%B4nio_%281974%29.jpg/230px-Marco_Ant%C3%B4nio_%281974%29.jpg'),
+        'Pelé': ('Édson Arantes do Nascimento', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Panini_pele_photo_only.jpg/160px-Panini_pele_photo_only.jpg'),
+        'Piazza': ('Wilson da Silva Piazza', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Wilson_Piazza.jpg/220px-Wilson_Piazza.jpg'),
+        'Roberto Miranda': ('Roberto Lopes de Miranda', 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Roberto_Miranda_%281970%29%2C_%27Mexico_70%27%2C_Panini_figurina.jpg'),
+        'Roberto Rivelino': ('Roberto Rivelino', 'https://upload.wikimedia.org/wikipedia/commons/d/df/Rivelino_brasil_figurita.jpg'),
+        'Tostão': ('Eduardo Gonçalves de Andrade', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Tost%C3%A3o_%28Eduardo_Gon%C3%A7alves_de_Andrade%2C_1970%29.jpg/640px-Tost%C3%A3o_%28Eduardo_Gon%C3%A7alves_de_Andrade%2C_1970%29.jpg'),
+        'Zé Maria': ('José Maria Rodrigues Alves', 'https://upload.wikimedia.org/wikipedia/commons/1/14/Z%C3%A9_Maria_1970.png')
+    }
+
+
+    if "selected_player" not in st.session_state:
+        st.header("Selecione um jogador:")
+
+
+        cols = st.columns(5)  
+        for i, (player, (name, image_url)) in enumerate(players.items()):
+            with cols[i % 5]:  
+                if st.button(player):  
+                    st.session_state.selected_player = player
+                    st.session_state.player_name = name
+                    st.session_state.player_image_url = image_url
+                    st.experimental_rerun()
+                player_image = load_and_resize_image(image_url)  
+                st.image(player_image, use_column_width=True)
+
+
+    else:
+        
+        selected_player = st.session_state.selected_player
+        player_name = st.session_state.player_name
+        player_image_url = st.session_state.player_image_url
+        
+        
+        if st.button("Voltar à página inicial"):
+            del st.session_state.selected_player
+            del st.session_state.player_name
+            del st.session_state.player_image_url
+            st.experimental_rerun()
+
+        
+        competitions = get_competitions()
+        world_cup_competitions = competitions[competitions['competition_name'] == 'FIFA World Cup']
+        season_data = world_cup_competitions[world_cup_competitions['season_name'] == '1970'].iloc[0]
+        competition_id = season_data['competition_id']
+        season_id = season_data['season_id']
+    
+       
+        player_events = get_player_events_competition(competition_id, season_id, player_name)
+    
+        if player_events.empty:
+            st.write(f"Nenhum evento encontrado para {player_name} na competição selecionada.")
+            return
+    
+       
+        passes = player_events[player_events['type'] == 'Pass']
+        assists = passes[passes['pass_goal_assist'] == True]
+        total_assists = len(assists)
+
+    
+        shots = player_events[player_events['type'] == 'Shot']
+        goals = shots[shots['shot_outcome'] == 'Goal']
+        total_goals = len(goals)
+        total_xg = shots['shot_statsbomb_xg'].sum().round(2)
+    
+
+        total_xt_pass, total_xt_carries = calculate_xT(player_events)
+
+        st.header(f'{selected_player}')
+    
+        stats_container = st.container()
+        with stats_container:
+            cols = st.columns(6)
+
+            with cols[0]:
+                player_image = load_and_resize_image(player_image_url, final_size = (300,500), aspect_ratio=3/5)  #
+                st.image(player_image)
+                
+            with cols[1]:
+                st.metric(label="Gols", value=int(total_goals))
+                st.metric(label="Assistências", value=int(total_assists))
+                st.metric(label="xG", value=total_xg)
+                st.metric(label="Pass xT", value=total_xt_pass.round(2))
+                st.metric(label="Carry xT", value=total_xt_carries.round(2))
+            with cols[2]:
+                st.subheader('Recepçoes')
+                fig_rec_actions = plot_reception_actions(player_events)
+                cropped_rec_actions = crop_figure(fig_rec_actions)
+                st.image(cropped_rec_actions,  width = 190)
+            with cols[3]:
+                st.subheader('Conduções')
+                fig_carries = plot_carries(player_events)
+                cropped_carries = crop_figure(fig_carries)
+                st.image(cropped_carries, width = 190)
+            with cols[4]:
+                st.subheader('Passes')
+                fig_passes = plot_passes(player_events)
+                cropped_passes = crop_figure(fig_passes)
+                st.image(cropped_passes, width = 190 )
+            with cols[5]:
+                st.subheader('Finalizações')
+                fig_possession_losses = plot_shots(player_events)
+                cropped_shots = crop_figure(fig_possession_losses)
+                st.image(cropped_shots,  width = 190)
+    
+ 
+       
+        st.markdown("""
+            <style>
+            .stContainer {
+                padding: 0em 0em;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,276 @@
+# utils.py
+from statsbombpy import sb
+import pandas as pd
+from mplsoccer import VerticalPitch as Pitch
+from PIL import Image
+import requests
+from io import BytesIO
+import numpy as np
+
+
+    
+def calculate_xT(player_events):
+    
+    
+
+    xt_url = 'https://karun.in/blog/data/open_xt_12x8_v1.json'
+    
+ 
+    response = requests.get(xt_url)
+    xt_grid = np.array(response.json())
+
+    def get_xt_value(x, y, xt_grid):
+        grid_x = min(int(x / (120 / 12)), 11)
+        grid_y = min(int(y / (80 / 8)), 7)
+        return xt_grid[grid_y, grid_x]
+    
+
+    player_events['xT_start'] = np.nan
+    player_events['xT_end'] = np.nan
+    player_events['xT_delta'] = np.nan
+    
+
+    for idx, event in player_events.iterrows():
+        if (event['type'] == 'Carry') or (pd.isna(event.get('pass_outcome')) and event['type'] == 'Pass'):
+
+            start_location = event['location']
+            end_location = event['pass_end_location'] if event['type'] == 'Pass' else event['carry_end_location']
+    
+
+            if start_location and end_location:
+                x_start, y_start = start_location
+                x_end, y_end = end_location
+
+                xT_start = get_xt_value(x_start, y_start, xt_grid)
+                xT_end = get_xt_value(x_end, y_end, xt_grid)
+                xT_delta = xT_end - xT_start
+    
+                # Atualizar o DataFrame
+                player_events.at[idx, 'xT_start'] = xT_start
+                player_events.at[idx, 'xT_end'] = xT_end
+                player_events.at[idx, 'xT_delta'] = xT_delta
+    
+
+    xt_passes = player_events[player_events['type'] == 'Pass']['xT_delta'].fillna(0).sum()
+    xt_carries = player_events[player_events['type'].isin(['Carry'])]['xT_delta'].fillna(0).sum()
+    
+    return xt_passes,xt_carries
+
+def crop_figure(fig, height_crop_percent=0.05,width_crop_percent = 0.275):
+    # Salvar o gráfico temporariamente em um buffer
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)
+    
+
+    img = Image.open(buf)
+    width, height = img.size
+    
+
+    left = int(width * width_crop_percent)
+    top = int(height * height_crop_percent)
+    right = width - left
+    bottom = height - top
+    
+    # Recortar a imagem
+    img = img.crop((left, top, right, bottom))
+    
+
+    return img
+
+def load_and_resize_image(url, final_size=(128, 160), aspect_ratio = 4/5):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    
+
+    width, height = img.size
+    
+
+    
+    if width / height > aspect_ratio:
+
+        new_width = int(height * aspect_ratio)
+        new_height = height
+    else:
+
+        new_width = width
+        new_height = int(width / aspect_ratio)
+    
+
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
+    
+
+    img = img.crop((left, top, right, bottom))
+    img = img.resize(final_size, Image.LANCZOS)
+    
+    return img
+
+
+
+
+def get_competitions():
+    competitions = sb.competitions()
+    return competitions
+
+def get_matches(competition_id, season_id):
+    matches = sb.matches(competition_id=competition_id, season_id=season_id)
+    return matches
+
+def get_events_competition(competition_id, season_id):
+    matches = get_matches(competition_id, season_id)
+    events_list = []
+
+    for _, match in matches.iterrows():
+        events_df = sb.events(match_id=match['match_id'], flatten_attrs=True)
+ 
+        events_list.append(events_df)
+
+    if events_list:
+        all_events = pd.concat(events_list)
+        return all_events
+    else:
+        return pd.DataFrame()
+def get_player_events_competition(competition_id, season_id, player_name):
+    matches = get_matches(competition_id, season_id)
+    player_events_list = []
+
+    for _, match in matches.iterrows():
+        events_df = sb.events(match_id=match['match_id'], flatten_attrs=True)
+        player_events = events_df[events_df['player'] == player_name]
+        if not player_events.empty:
+            player_events_list.append(player_events)
+
+    if player_events_list:
+        all_player_events = pd.concat(player_events_list)
+        return all_player_events
+    else:
+        return pd.DataFrame()
+
+def plot_passes(events_df, title=''):
+    pitch = Pitch()
+    fig, ax = pitch.draw()
+
+    passes = events_df[(events_df['type'] == 'Pass') & (events_df['pass_outcome'].isna())]
+
+    if passes.empty:
+        return fig
+
+    pass_start_x = passes['location'].apply(lambda loc: loc[0])
+    pass_start_y = passes['location'].apply(lambda loc: loc[1])
+    pass_end_x = passes['pass_end_location'].apply(lambda loc: loc[0])
+    pass_end_y = passes['pass_end_location'].apply(lambda loc: loc[1])
+    
+    pitch.arrows(pass_start_x, pass_start_y, pass_end_x, pass_end_y, ax=ax, width=1,  color='black', alpha = 0.2)
+    
+    key_passes = passes[(passes['xT_delta'] >= 0.05)]
+    
+    if key_passes.empty:
+        return fig
+    pass_start_x = key_passes['location'].apply(lambda loc: loc[0])
+    pass_start_y = key_passes['location'].apply(lambda loc: loc[1])
+    pass_end_x = key_passes['pass_end_location'].apply(lambda loc: loc[0])
+    pass_end_y = key_passes['pass_end_location'].apply(lambda loc: loc[1])
+    
+    pitch.arrows(pass_start_x, pass_start_y, pass_end_x, pass_end_y, ax=ax, width=2,  color='black', alpha = 0.8)
+    ax.set_title(title)
+
+    return fig
+
+def plot_carries(events_df, title=''):
+    pitch = Pitch(pitch_type='statsbomb')
+    fig, ax = pitch.draw()
+
+    carries = events_df[events_df['type'] == 'Carry']
+
+    if carries.empty:
+        return fig
+
+    carry_start_x = carries['location'].apply(lambda loc: loc[0])
+    carry_start_y = carries['location'].apply(lambda loc: loc[1])
+    carry_end_x = carries['carry_end_location'].apply(lambda loc: loc[0])
+    carry_end_y = carries['carry_end_location'].apply(lambda loc: loc[1])
+
+    pitch.arrows(carry_start_x, carry_start_y, carry_end_x, carry_end_y, ax=ax, width=1.5, color='black', alpha = 0.2)
+    
+    
+    key_carries = carries[carries['xT_delta'] >= 0.025]
+    if key_carries.empty:
+        return fig
+    carry_start_x = key_carries['location'].apply(lambda loc: loc[0])
+    carry_start_y = key_carries['location'].apply(lambda loc: loc[1])
+    carry_end_x = key_carries['carry_end_location'].apply(lambda loc: loc[0])
+    carry_end_y = key_carries['carry_end_location'].apply(lambda loc: loc[1])
+    
+    pitch.arrows(carry_start_x, carry_start_y, carry_end_x, carry_end_y, ax=ax, width=2,  color='black', alpha = 0.8)
+    ax.set_title(title)
+
+    return fig
+
+
+
+
+def plot_reception_actions(events_df, title=''):
+
+    pitch = Pitch(line_zorder=2)
+
+    fig, ax = pitch.draw()
+
+
+    reception_types = ['Ball Receipt*']
+    reception_events = events_df[events_df['type'].isin(reception_types)]
+
+    if reception_events.empty:
+        return fig
+
+
+    x = reception_events['location'].apply(lambda loc: loc[0])
+    y = reception_events['location'].apply(lambda loc: loc[1])
+
+
+    x = np.array(x)
+    y = np.array(y)
+
+
+    pitch.kdeplot(x, y, ax=ax, cmap='Greys', alpha=1, thresh=0.5, shade=True,
+                  levels=100, n_levels=10, cut=1, zorder=1, fill=True)
+
+    ax.set_title(title)
+    return fig
+
+
+
+def plot_shots(events_df, title=''):
+    pitch = Pitch()
+    fig, ax = pitch.draw()
+
+    shots = events_df[events_df['type'] == 'Shot']
+
+    if shots.empty:
+        return fig
+
+
+    goals = shots[shots['shot_outcome'] == 'Goal']
+    non_goals = shots[shots['shot_outcome'] != 'Goal']
+
+
+
+    if not non_goals.empty:
+        non_goal_x = non_goals['location'].apply(lambda loc: loc[0])
+        non_goal_y = non_goals['location'].apply(lambda loc: loc[1])
+        non_goal_s = non_goals['shot_statsbomb_xg'].apply(lambda x: max(x, 0.075)*250)
+        pitch.scatter(non_goal_x, non_goal_y, ax=ax, edgecolors='black', c='black', s=non_goal_s, alpha = 0.25)
+
+
+    if not goals.empty:
+        goal_x = goals['location'].apply(lambda loc: loc[0])
+        goal_y = goals['location'].apply(lambda loc: loc[1])
+        goal_s = goals['shot_statsbomb_xg'].apply(lambda x: max(x, 0.075)*250)
+        pitch.scatter(goal_x, goal_y, ax=ax, edgecolors='black', c='black', s=goal_s, alpha = 1)
+
+    ax.set_title(title)
+
+
+    return fig
